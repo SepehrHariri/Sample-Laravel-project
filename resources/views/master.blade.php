2465e6e916438996b2afa08b8365e4de8b03eaff
SepehrHariri <sepehrhariry@gmail.com>
2021-03-05 19:13:55 +0330

    master1

---
+++ resources/views/master.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Livewire</title>
+        <livewire:styles />
+    </head>
+    <body>
+        <h3>Livewire</h3>
+        <livewire:scripts />
+    </body>
+</html>
