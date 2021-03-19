12430173fbe98f3355341721f66130b9531beb6d
SepehrHariri <sepehrhariry@gmail.com>
2021-03-19 14:11:09 +0330

    layouts.app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Livewire</title>
+        <livewire:styles />
+    </head>
+    <body>
+        <h3>Livewire</h3>
+
+{{ $slot }}
+
+        <livewire:scripts />
+    </body>
+</html>
